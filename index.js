0e1da296dcb63e84faaea8f3719f3aa085b1ef74
Sonia012 <Sonia.Testaert@telenet.be>
2019-02-20 16:17:58 +0100

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require("./routes/quoteRoutes")(app);
 mongoose.connect("mongodb+srv://root:root@becode-taakq.mongodb.net/quotesDB",{useNewUrlParser: true});
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running`);
