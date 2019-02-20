470b87953880121ce2bd360f4a684fc28475bd47
Sonia012 <Sonia.Testaert@telenet.be>
2019-02-20 15:21:23 +0100

    make it better

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(`public`));
 
 require("./routes/quoteRoutes")(app);
 
